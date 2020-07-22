9ac5870104c7d38404b2826aeb5d70e0a9494109
Zoe <zohommerich@davidson.edu>
2020-07-22 11:12:49 -0400

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -22,11 +22,28 @@ function addName() {
   // - https://www.w3schools.com/jsref/prop_node_innertext.asp
 
   let nameElement = document.querySelector('#name');
-  nameElement.innerText = 'See script.js!';
+  nameElement.innerText = 'Jody!';
 }
 
 function addFavoriteThings() {
   console.log('Called addFavoriteThings()');
+    let favthingsElement = document.querySelector('#favthings');
+
+    let sleep = document.createElement('li');
+    sleep.textContent = 'sleeping';
+    favthingsElement.appendChild(sleep);
+
+    let food = document. createElement('li');
+    food.textContent = 'food';
+    favthingsElement.appendChild(food);
+
+    let cuddles = document.createElement('li');
+    cuddles.textContent = 'cuddles';
+    favthingsElement.appendChild(cuddles);
+
+    let walks = document.createElement('li');
+    walks.textContent = 'walks';
+    favthingsElement.appendChild(walks);
 
   // 1. Get a reference to <ul id="favthings">
   // 2. Create a few list items representing your favorite things
@@ -41,6 +58,8 @@ function addFavoriteThings() {
 function replaceImage() {
   console.log('Called replaceImage()');
 
+    let newDoggo = document.querySelector('#picture');
+    newDoggo.setAttribute('src', 'http://buzzsharer.com/wp-content/uploads/2015/04/Black-puppy-lab.jpg');
   // Change the puppy picture to a picture of your choosing
 
   // See:
@@ -51,6 +70,13 @@ function replaceImage() {
 function changeCodeStatus() {
   console.log('Called changeCodeStatus()');
 
+  let status = document.querySelector('#codestatus');
+  status.innerHTML = '<img src="https://media.tenor.com/images/1cc2bf095b801521ac22418c216d50f6/tenor.gif">';
+  // status = document.createElement('img');
+  //status.setAttribute('src','https://media.tenor.com/images/1cc2bf095b801521ac22418c216d50f6/tenor.gif');
+  // codestatus.appendChild(status);
+  // This just creates an image and adds the image below the text, but does NOT replace it
+
   // 1. Get a reference to <div id="codestatus">
   // 2. Create image element containing a sweet ol' meme
   // 3. Replace text in codestatus w/ image
@@ -59,7 +85,7 @@ function changeCodeStatus() {
 // Get a reference to the button w/ id="show-info-button"
 let showInfoButton = document.querySelector('#show-info-button');
 
-// Do something when showInfoButton is clicke
+// Do something when showInfoButton is clicked
 showInfoButton.addEventListener('click', function() {
   console.log('Clicked "Show Info" button');
 
@@ -86,18 +112,53 @@ let informationForm = document.querySelector('#information-form');
 
 // Do something when form is submitted
 informationForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // You will want this here. Remove it and see what changes.
+    event.preventDefault(); // prevents page from opening to a link on click
+    console.log('Form submitted');
+    //let fname = document.querySelector('#fname').value; brauch man nicht wirklich, ist nur dar zum Referencing
+    //let lname = document.querySelector('#lname').value; dasselbe für hier
+    document.querySelector('#firstname').innerHTML = document.querySelector('#fname').value;
+    document.querySelector('#lastname').innerHTML = document.querySelector('#lname').value;
+    document.querySelector ('#chosencar').innerHTML = document.querySelector('#cars').value;
 
-  console.log('Form submitted');
+    let iceCreamY = document.querySelector('#icecreamyes').checked;
+    let iceCreamN = document.querySelector('#icecreamno').checked;
+
+    //could have also just written the strings 'Yes' or 'No' inside .append()
+    if (iceCreamY) {
+      document.querySelector('#icecreamstatus').append(document.querySelector('#icecreamyes').value);
+    };
+
+    if (iceCreamN) {
+      document.querySelector('#icecreamstatus').append(document.querySelector('#icecreamno').value);
+    };
+
+    let isHuman = document.querySelector('#humancheck').checked;
+    let isCoder = document.querySelector('#codercheck').checked;
+
+    if (isHuman) {
+      document.querySelector('#checks').append('This person is a human.');
+    }
+    if (isCoder) {
+      document.querySelector('#checks').append('This person is a coder.');
+    }
 
   // Your job:
-  //   1. Get information typed into the form
+  //   1. Get information typed into the form"
   //   2. Display that info in "Display that info here" section
 
   // Google things like:
   //   javascript form element get values
 });
 
+//first try:
+//function(event) doomed(){
+  //var fname = document.querySelector('fname');
+  //var firstname = document.querySelector('firstname');
+  //  firstname.innerHTML = fname;
+  //let jsfirstname = document.querySelector("firstname");
+  //jsfirstname.textContent = John;
+  //fristname.appendChild(John)
+//}
 /*************************************
  * Section 3 - Clicks and Keypresses *
  *************************************
@@ -111,15 +172,17 @@ informationForm.addEventListener('submit', function(event) {
 // then log something to the console
 
 // Fill in ________ to get a reference to the correct button on the page
-let consoleLogButton = document.querySelector('#________');
+let consoleLogButton = document.querySelector('#console-log-button');
 
 // Log something when that button is clicked
 consoleLogButton.addEventListener('click', function() {
-  console.log('Change this text if you want!');
+  console.log('Someone dared to click this button?');
+  document.querySelector('#logText').innerHTML='All I do is log log log no matter what.';
 });
 
-let makeBlueButton = document.querySelector('#________');
+let makeBlueButton = document.querySelector('#make-blue-button');
 makeBlueButton.addEventListener('click', function() {
+  document.querySelector('#colorText').style.color='blue';
   // Your job:
   //  1. When a user clicks "Change the text to the right blue"
   //  2. Change the text in <div id="colorText">...</div> to blue
@@ -127,9 +190,15 @@ makeBlueButton.addEventListener('click', function() {
 
 // Adding an event listener to document means the "keydown" event
 // can happen anywhere on the page and we'll respond.
-document.addeventListener('keydown', function() {
+document.addEventListener('keydown', function() {
   // This is called whenever a user pressed any key.
+  if (event.code == 'KeyR'){
+    document.querySelector('#colorText').style.color = 'red';
+  }
 
+  if (event.code >= 'Digit0' && event.code <= 'Digit9' ){
+    alert('You have entered a digit. Digits are too uncool for Jody. Please enter a valid character. (e.g. valid characters are characters that could spell my name)')
+  };
   // Your job:
   //  1. Turn colorText red whenever a user presses the 'r' key
   //  2. Add your own keybind somewhere
@@ -138,6 +207,8 @@ document.addeventListener('keydown', function() {
   // - https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event
   // - https://javascript.info/keyboard-events
 });
+
+
 
 /*************************************
  * Section 4 - TODO List             *
@@ -155,7 +226,18 @@ document.addeventListener('keydown', function() {
  */
 
 // Your code goes here
+let addItems = document.querySelector('.form');
 
+addItems.addEventListener('submit', function(){
+  event.preventDefault(); // Terra added this in section 2, go look there if confused
+  console.log('Item added'); //to keep track of it
+
+  let newToDoItem = document.createElement('li');
+  newToDoItem.textContent = document.querySelector('#toDoItem').value;
+
+  let toDoList = document.querySelector('#todos');
+  toDoList.appendChild(newToDoItem);
+});
 /****************************************
  * Section 5 - setInterval + setTimeout *
  ****************************************
@@ -174,7 +256,19 @@ document.addeventListener('keydown', function() {
  */
 
 // Your code goes here
+let counter = 0;
+let secondTimer = setInterval( function(){
+    counter = counter + 1;
+    let counting = document.querySelector('#seconds');
+    counting.innerHTML = counter + " " + 'seconds';
+  }, 1000,);
 
+let delayedTimeout = setTimeout(function(){
+    console.log('you did it!');
+    document.querySelector('#delayedDisplay').innerHTML = 'Whoa what is happening here?';
+    document.querySelector('#delayedDisplay').style.color = 'yellow';
+    document.querySelector('#delayedDisplay').style.fontWeight = 'bold';
+}, 5000)
 /****************************************
  * Section 6 - Your own section!        *
  ****************************************
